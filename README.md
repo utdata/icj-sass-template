f0c8951a43efd3802a26b175d5855e3c6cc46c4c
Christian McDonald <christian.mcdonald@utexas.edu>
2018-10-06 17:42:49 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Lesson to refactor CSS to SCSS
+
+## Set up the project
+
+- Download the repo (don't clone) into your `/icj` folder.
+- Unzip the file and rename the resulting folder `yourname-sass`.
+- Open the folder in VS Code
+- Go to github and create a new repo also called `yourname-sass`. DO NOT include the README.
+- The resulting directions in Github should be performed in your VS Code terminal.
+- Push to Github to make sure they are connected. When you refresh your github repo, you should see the files.
+
+## npm setup
+
+- run `npm install` in your terminal to set up the development environment.
+
+
+
---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src(['src/scss/*.scss'])
+        .pipe(sass())
+        .pipe(gulp.dest("docs/css"))
+        .pipe(browserSync.stream());
+});
+
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./docs"  
+    });
+
+    gulp.watch(['src/scss/*.scss'], ['sass']);
+    gulp.watch("docs/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['serve']);
