e7980dca3c2006686f6f9a27974cf1e5fe492bac
Christian McDonald <christian.mcdonald@utexas.edu>
2018-10-10 14:09:02 -0500

    adding error logging to sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
+var gutil       = require('gulp-util');
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
     return gulp.src(['src/scss/*.scss'])
         .pipe(sass())
+        .on('error', gutil.log)
         .pipe(gulp.dest("docs/css"))
         .pipe(browserSync.stream());
 });
