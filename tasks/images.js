e4a19ce5ea71b326e584dad70fbcfc728aa5f9be
Christian McDonald <christian.mcdonald@utexas.edu>
2019-08-14 22:39:26 -0500

    removing imagemin-giflossy because it is out of date and we aren't using it

--- tasks/images.js
+++ tasks/images.js
@@ -2,7 +2,6 @@ const gulp = require('gulp');
 const cache = require('gulp-cache');
 const imagemin = require('gulp-imagemin');
 const imageminMozjpeg = require('imagemin-mozjpeg'); 
-const imageminGiflossy = require('imagemin-giflossy');
 const imageminPngquant = require('imagemin-pngquant');
 const imageminWebp = require('imagemin-webp');
 
@@ -17,12 +16,6 @@ module.exports = () => {
       imageminPngquant({
         speed: 1,
         quality: 98 //lossy settings
-      }),
-      // gif very light lossy.
-      imageminGiflossy({
-        optimizationLevel: 3,
-        optimize: 3, //keep-empty: Preserve empty transparent frames
-        lossy: 2
       }),
       // webp google givs
       imageminWebp({
