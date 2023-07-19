1c822f407cf64d30329acf1b269d320a29960985
Christian McDonald <christian.mcdonald@utexas.edu>
2023-07-19 14:59:38 -0500

    adding ability to nest html

--- tasks/html.js
+++ tasks/html.js
@@ -2,7 +2,10 @@ import gulp from 'gulp';
 
 function html() {
   return gulp
-    .src(['src/*.html'])
+    .src([
+      'src/*.html',
+      'src/**/*.html'
+    ])
     .pipe(gulp.dest('./docs'));
 }
 
